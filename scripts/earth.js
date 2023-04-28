2c58f6fb4b0c7407bd07a6e6ffdd2f29d17fdcc2
Daniel Greenheck <dgreenheck@gmail.com>
2023-04-28 11:55:38 -0500

    Moved photos to ThreeJS

--- scripts/earth.js
+++ scripts/earth.js
@@ -1,16 +1,7 @@
 import * as THREE from 'three';
-import * as geoData from '../images/geoData.json';
 
 const rad2deg = (r) => r * (180 / Math.PI);
 const deg2rad = (d) => d / (180 / Math.PI);
-
-const images = [
-  'images/bahamas.jpg',
-  'images/italy.jpg',
-  'images/newyork.jpg',
-  'images/paris.jpg',
-  'images/newzealand.jpg'
-];
 
 // Load resources
 const earthTexture = new THREE.TextureLoader().load('images/earth.jpg');
@@ -26,6 +17,8 @@ document.body.appendChild(renderer.domElement);
 const scene = new THREE.Scene();
 
 const camera = new THREE.PerspectiveCamera(60, window.innerWidth / window.innerHeight, 0.1, 1000);
+camera.position.x = 3;
+camera.lookAt(0, 0, 0);
 
 const earthGeometry = new THREE.SphereGeometry(1, 32, 32);
 const earthMaterial = new THREE.MeshPhongMaterial({ 
@@ -34,24 +27,48 @@ const earthMaterial = new THREE.MeshPhongMaterial({
   bumpScale: 0.05,
   specular: 0x333333
 });
-
 const earth = new THREE.Mesh(earthGeometry, earthMaterial);
-sphere.rotation.order = 'ZYX';
-scene.add(sphere);
+earth.rotation.order = 'ZYX';
 
-for (const image of images) {
-  const imageTexture = new THREE.TextureLoader().load(image);
-  const imageGeometry = new THREE.PlaneGeometry();
-  const imageMaterial = new THREE.MeshPhongMaterial({ map: imageTexture })
-  const imageMesh = new THREE.Mesh(imageGeometry, imageMaterial);
+let imageMeshes = [];
+
+function loadPhotos(photos) {
+  scene.clear();
+
+  scene.add(earth);
+
+  imageMeshes = [];
+
+  for (const [imageUrl, coords] of Object.entries(photos)) {
+    const imageTexture = new THREE.TextureLoader().load(imageUrl);
+    const imageGeometry = new THREE.PlaneGeometry(0.2, 0.2);
+    const imageMaterial = new THREE.MeshPhongMaterial({ map: imageTexture })
+    const imageMesh = new THREE.Mesh(imageGeometry, imageMaterial);
+    imageMeshes.push(imageMesh);
+
+    // Then rotate to it's lat/lon
+    // First translate the image in the +Z direciton
+    const translateNode = new THREE.Object3D();
+    translateNode.position.x = 1.01;
+    translateNode.rotation.y = deg2rad(90);
+    translateNode.add(imageMesh);
+
+    const rotateNode = new THREE.Object3D();
+    rotateNode.rotation.set(0, -deg2rad(coords.lon), deg2rad(coords.lat));
+    rotateNode.add(translateNode);
+
+    earth.add(rotateNode);
+  }
+
+  const ambient = new THREE.AmbientLight(0xffffff, 0.3);
+  scene.add(ambient);
+
+  const sun = new THREE.DirectionalLight(0xffffff, 0.3);
+  sun.position.set(1, 1, 1);
+  scene.add(sun);
+
+  animate();
 }
-
-const light = new THREE.DirectionalLight(0xffffff, 0.3);
-light.position.set(1, 1, 1);
-scene.add(light);
-
-camera.position.x = 2.5;
-camera.lookAt(0, 0, 0);
 
 function animate() {
   requestAnimationFrame(animate);
@@ -60,8 +77,8 @@ function animate() {
 
 function getLatLon() {
   return {
-    lat: rad2deg(-sphere.rotation.z),
-    lon: rad2deg(sphere.rotation.y)
+    lat: rad2deg(-earth.rotation.z),
+    lon: rad2deg(earth.rotation.y)
   }
 }
 
@@ -97,20 +114,22 @@ function setLatLon(lat, lon) {
     lon += 360;
   }
 
-  sphere.rotation.z = deg2rad(-lat);
-  sphere.rotation.y = deg2rad(lon);
+  earth.rotation.z = deg2rad(-lat);
+  earth.rotation.y = deg2rad(lon);
 }
 
-function setZoom(zoom) {
-  camera.position.x = 2.5 / zoom * 0.2;
-  console.log(camera.position.x);
-}
+function updateZoom(deltaZoom) {
+  camera.position.x = Math.max(1.1, Math.min(camera.position.x + deltaZoom, 5));
 
-animate();
+  for (const mesh of imageMeshes) {
+    mesh.scale.set(camera.position.x / 3, camera.position.x / 3, 1);
+  }
+}
 
 export default {
+  loadPhotos,
   getLatLon,
   updateLatLon,
   setLatLon,
-  setZoom
+  updateZoom
 }
